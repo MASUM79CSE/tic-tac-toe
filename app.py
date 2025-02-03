1d3ace196bd7b19a883776c06cdd8f03143048fb
Mir Md. Masum <bubt.cse.masum@gmail.com>
2025-02-04 00:08:38 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# 404 Error Handler
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# 500 Error Handler
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
